6202667ccb835620158d5fda011558332c8629fd
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-01 16:13:58 +0900

    Initial Commit

---
+++ ScreenTransition2/ScreenTransition2App.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTransition2App.swift
+//  ScreenTransition2
+//
+//  Created by emi oiso on 2023/10/01.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTransition2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
